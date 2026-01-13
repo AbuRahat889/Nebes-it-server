416b2e2e11a175a3ab9e76f5f5959301317b8ab9
AbuRahat889 <146001390+AbuRahat889@users.noreply.github.com>
2026-01-13 17:03:18 +0600

    Refactor prisma client initialization and update vercel configuration to include environment variables

--- src/app/utils/prisma.ts
+++ src/app/utils/prisma.ts
@@ -1,5 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+// export default prisma;
+
+import { PrismaClient } from '@prisma/client';
+
+declare global {
+  // allow global `prisma` for hot reload in development
+  var prisma: PrismaClient | undefined;
+}
+
+// explicitly type `prisma`
+const prisma: PrismaClient = global.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+
 export default prisma;
-
